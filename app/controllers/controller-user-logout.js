086ad9d087661210b6f7afcd16990ff088fb7498
Krzysztof Wielicki <kwielicki@e-point.pl>
2018-02-17 22:24:52 +0100

    PLANNER-043 Permission denied podczas wylogowywania a aplikacji

--- app/controllers/controller-user-logout.js
+++ app/controllers/controller-user-logout.js
@@ -6,9 +6,18 @@
 
 weddingPlanner.controller('ctrlUserLogout', function( $scope, $window ) {
     $scope.appSignOut = function() {
+        /* @TODO tuż przed wylogowaniem należy zniszczyć wszelkie
+         * powiązania do referencyjnych tablic Firebas'a korzystając z
+         * metody $destroy(). W innym przypadku, w momencie wylogowywania z widoku
+         * na którym jest wykonywana jakakolwiek zależność z bazą występuje błąd:
+         * np. Error: permission_denied at /to_do_list_category: Client doesn't have permission to access the desired data.
+         * To jest znany błąd nie wpływający na działanie aplikacji.
+         */
         firebase.auth().signOut()
             .then(function() {
                 $window.location.href = "/#/login";
+            }).catch(function(err) {
+                console.log(err);
             });
     }
 });
