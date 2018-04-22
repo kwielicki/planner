f93f28011854ee305c579b4d0fdf0098bb42c070
kwielicki <krzysztofwielicki86@gmail.com>
2018-04-22 12:23:50 +0200

    PLANNER-063 Dodanie komponentu intro-banner

---
+++ app/components/app-intro-banner.js
@@ -0,0 +1,31 @@
+angular
+    .module('weddingPlanner')
+    .component('appIntroBanner', {
+        templateUrl: 'templates/components/component-intro-banner.html',
+        bindings: {
+         introbannerimage: '@',
+		 introbannertitle: '@',
+		 introbannerdescription: '@'
+        },
+        controller: function( $scope, $element, $window ) {
+
+            const ctrl = this;
+
+            // Klasy pomocnicze
+            ctrl.classHelpers = {
+                componentName: 'intro-banner',
+				componentInitialized: 'intro-banner--initialized'
+            };
+
+            // Dodanie klasy oraz atrybutu na komponent
+            ctrl.$onInit = () => {
+                $element
+                    .addClass(`
+                        ${ctrl.classHelpers.componentName}
+						${ctrl.classHelpers.componentInitialized}
+                    `)
+					.attr('data-component-name', ctrl.classHelpers.componentName);
+            };
+
+        }
+    })
