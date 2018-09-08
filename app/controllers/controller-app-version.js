bbcae0f7d47fa399fd343c0660b1beae4e0562ce
kwielicki <krzysztofwielicki86@gmail.com>
2018-09-08 23:16:40 +0200

    Wydanie wersji 1.1.0, uzupełnienie dokumentacji

--- app/controllers/controller-app-version.js
+++ app/controllers/controller-app-version.js
@@ -1,5 +1,5 @@
 angular
     .module('weddingPlanner')
     .controller('controllerAppVersion', [ '$scope', function ( $scope ) {
-        $scope.appVersion = '1.0.9';
+        $scope.appVersion = '1.1.0';
     }]);
