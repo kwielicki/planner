012e5b720634900b550588a62aaec0ee24c87d50
kwielicki <krzysztofwielicki86@gmail.com>
2018-10-19 23:05:53 +0200

    PLANNER-107 Przebudowa nawigacji (Etap 1)

--- app/components/comp-header.js
+++ app/components/comp-header.js
@@ -2,7 +2,7 @@ angular
 	.module('weddingPlanner')
 	.component('compHeader', {
 		templateUrl: 'templates/components/component-header.html',
-		controller: function($scope, $element, $rootScope, $location, $localStorage, Auth) {
+		controller: function($scope, $element, $rootScope, $location, $localStorage, Auth, $window) {
 
 			/* Controler object for compHeader */
 			const ctrl = this;
@@ -49,68 +49,88 @@ angular
 				 * - returning false if menu is closed
 				 */
 				if (open === true) {
-					ctrl.toggleMenuStatus = "Zamknij menu";
+					console.log('Menu opener');
 				} else {
-					ctrl.toggleMenuStatus = "Otwórz menu";
+					console.log('Mennu closer');
 				}
 			}
 
+			/* Dropdown menu will be closed after click on any list element */
+			ctrl.sideBarMenuStatus = false;
+			ctrl.sideBarMenuHidding = function( viewLocation ) {
+				ctrl.sideBarMenuStatus = false;
+				$location.path(viewLocation);
+			};
 
 			/* Static models for menu dropdown renderers */
-			ctrl.menuItems__dashboard = [{
-				name: 'Panel główny',
-				url: '/dashboard',
-				title: 'Przejdź do panelu głównego'
-			}];
-
+			ctrl.pathToSvg = "../../assets/images/svg/";
 			ctrl.menuItems__modules = [{
+					name: 'Panel główny',
+					url: '/dashboard',
+					title: 'Przejdź do panelu głównego',
+					icon: '../../assets/images/svg/puzzle.svg'
+				},
+				{
 					name: 'Zarządzaj listą gości',
 					url: '/manage-guests',
-					title: 'Zarządzaj listą gości'
+					title: 'Zarządzaj listą gości',
+					icon: '../../assets/images/svg/networking.svg'
 				},
 				{
 					name: 'Dodaj nowego gościa',
 					url: '/add-new-guest',
-					title: 'Dodaj nowego gościa'
+					title: 'Dodaj nowego gościa',
+					icon: '../../assets/images/svg/interview.svg'
 				},
 				{
 					name: 'Organizer weselny',
 					url: '/wedding-organizer',
-					title: 'Organizer weselny'
+					title: 'Organizer weselny',
+					icon: '../../assets/images/svg/chat.svg'
 				},
 				{
 					name: 'Notatnik weselny',
 					url: '/wedding-notebook',
-					title: 'Notatnik weselny'
+					title: 'Notatnik weselny',
+					icon: '../../assets/images/svg/document.svg'
 				},
 				{
 					name: 'Lista rzeczy do zrobienia',
 					url: '/todo-list',
-					title: 'Lista rzeczy do zrobienia'
+					title: 'Lista rzeczy do zrobienia',
+					icon: '../../assets/images/svg/task.svg'
 				},
 				{
 					name: 'Zarządzaj wydatkami',
 					url: '/manage-expenses',
 					title: 'Zarządzaj wydatkami',
-					specialClass: 'not--available'
+					specialClass: 'not--available',
+					icon: '../../assets/images/svg/profit.svg'
 				},
 				{
 					name: 'Statystyki',
 					url: '/statistics',
-					title: 'Zapoznaj się ze statystykami'
-				},
+					title: 'Zapoznaj się ze statystykami',
+					icon: '../../assets/images/svg/presentation.svg'
+				}
+			];
+
+			ctrl.menuItems__doc = [
 				{
 					name: 'Dokumentacja',
 					url: '/documentation',
-					title: 'Dokumentacja aplikacji Wedding Planner'
+					title: 'Dokumentacja aplikacji Wedding Planner',
+					icon: '../../assets/images/svg/briefcase.svg'
 				}
 			];
+
 
 			ctrl.menuItems__additional = [
 				{
 					name: 'Aktualności',
 					url: '/news',
-					title: 'Aktualności'
+					title: 'Aktualności',
+					icon: '../../assets/images/svg/global.svg'
 				}
 			];
 
