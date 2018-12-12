cbcddfa963418a11e750dbc07cfa610df0e51d53
kwielicki <krzysztofwielicki86@gmail.com>
2018-12-12 22:41:11 +0100

    PLANNER-111 RWD Modala dot. usuwania procesu usuwania gościa

--- app/controllers/ctrl-delete-person.js
+++ app/controllers/ctrl-delete-person.js
@@ -9,8 +9,36 @@ angular
         $scope.deletePerson = function(personID) {
             const expectedPerson = $scope.persons.$getRecord(personID);
             $scope.animationsEnabled = true;
-
-            var templateModal = "\n            <div class=\"modal-planner__header\">\n                <h5 class=\"modal-planner__title\">Osoba kt\xF3ra zostanie usuni\u0119ta to -\n                    <span class=\"text-uppercase modal-planner__guest-name\">" + expectedPerson.firstName + " " + expectedPerson.surName + "</span>\n                    <i class=\"fa fa-question-circle\" title=\"" + expectedPerson.firstName + " " + expectedPerson.surName + " - dodany przez " + expectedPerson.creator + "\"></i>\n                </h5>\n            </div>\n            <div class=\"modal-body\">\n                <div ng-hide=\"whenDeleteGuestComplete\">\n                    <div class=\"modal-description text-xs-center\">\n                        <h4 class=\"modal-description-title\">Uwaga!</h4>\n                        <h5 class=\"modal-description-subtitle\">Dane dla tego go\u015Bcia zostan\u0119 trwale usuni\u0119te.</h5>\n                    </div>\n                    <div class=\"modal-buttons\">\n                        <a href=\"#\" ng-click=\"deletePermanent()\" class=\"btn btn-danger\">Usu\u0144 trwale</a>\n                        <a href=\"#\" ng-click=\"closeModal()\" class=\"btn btn-default\">Nie usuwaj</a>\n                    </div>\n                </div>\n                <div ng-show=\"whenDeleteGuestComplete\" class=\"hide text-xs-center\">" + expectedPerson.firstName + " " + expectedPerson.surName + " - go\u015B\u0107 usuni\u0119ty</div>\n                <div ng-show=\"whenDeleteGuestError\" class=\"hide text-xs-center\">Oops. Wyst\u0105pi\u0142 b\u0142\u0105d. Skontaktuj si\u0119 z administratorem.</div>\n\n            </div>\n            <a href=\"#\" class=\"modal-close\" title=\"Zamknij okno\" ng-click=\"closeModal()\">Zamknij</a>\n        ";
+            let templateModal = `
+                    <div class="modal-planner__header">
+                        <div class="modal-planner__title">
+                            <span ng-hide="whenDeleteGuestComplete">Proces usuwania gościa (1/2)</span>
+                            <span ng-show="whenDeleteGuestComplete">Proces usuwania gościa (2/2)</span>
+                            <i class="fa fa-question-circle" title="${expectedPerson.firstName} ${expectedPerson.surName} został dodany przez ${expectedPerson.creator}."></i>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <div>
+                            <div class="modal-description text-xs-center" ng-hide="whenDeleteGuestComplete">
+                                <h4 class="modal-description-title">Uwaga!</h4>
+                                <h5 class="modal-description-subtitle">
+                                    Czy chcesz usunąć trwale dane dotyczące <br/> gościa&nbsp;<abbr>${expectedPerson.firstName}&nbsp;${expectedPerson.surName}</abbr>
+                                </h5>
+                            </div>
+                            <div class="modal-description text-xs-center hide" ng-show="whenDeleteGuestComplete">
+                                Gość, <abbr>${expectedPerson.firstName} ${expectedPerson.surName}</abbr> <br/> został usunięty z Twojej bazy danych.
+                            </div>
+                            <div class="modal-buttons" ng-hide="whenDeleteGuestComplete">
+                                <a href="#" ng-click="deletePermanent()" class="btn btn-material btn-xs">Usuń trwale</a>
+                                <a href="#" ng-click="closeModal()" class="btn btn-material btn-xs">Nie usuwaj</a>
+                            </div>
+                            <div class="modal-buttons" ng-show="whenDeleteGuestComplete">
+                                <a href="#" ng-click="closeModal()" class="btn btn-material btn-xs hide">Zamknij okno</a>
+                            </div>
+                        </div>
+                        <div ng-show="whenDeleteGuestError" class="hide text-xs-center">Oops. Wystąpił błąd. Skontaktuj się z administratorem.</div>
+                    </div>
+                `;
 
             var modalInstance = $uibModal.open({
                 animation: $scope.animationsEnabled,
@@ -18,7 +46,7 @@ angular
                 ariaLabelledBy: 'modal-title',
                 ariaDescribedBy: 'modal-body',
                 windowClass: 'modal--centered modal--shadowed modal-planner modal-planner--delete',
-                openedClass: 'modal-opened modal-overlay--light',
+                openedClass: 'modal-opened modal-overlay--dark',
                 size: 'md',
                 controller: function( $scope ) {
                     $scope.deletePermanent = function() {
