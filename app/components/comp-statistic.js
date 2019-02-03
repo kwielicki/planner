0863564ec555bc627b5f5e7d6be67cc5b324f65d
kwielicki <krzysztofwielicki86@gmail.com>
2019-02-03 15:02:28 +0100

    PLANNER-125 Statystyki ( wyodrębnienie do komponentu )

---
+++ app/components/comp-statistic.js
@@ -0,0 +1,53 @@
+angular
+    .module('weddingPlanner')
+    .component('compStatistic', {
+        templateUrl: 'templates/components/comp-statistic.html',
+        controller: function( $scope, $element, $window, $firebaseArray ) {
+
+            const ctrl = this;
+
+            // Klasy pomocnicze
+            ctrl.classHelpers = {
+                componentName: 'statistic',
+				componentInitialized: 'statistic--initialized'
+            };
+
+            // Dodanie klasy oraz atrybutu na komponent
+            ctrl.$onInit = () => {
+                $element
+                    .addClass(`
+                        ${ctrl.classHelpers.componentName}
+						${ctrl.classHelpers.componentInitialized}
+                    `)
+					.attr('data-component-name', ctrl.classHelpers.componentName);
+
+				const refListOfGuest = firebase.database().ref().child("list_of_guest");
+				ctrl.guestData = [];
+
+                $firebaseArray(refListOfGuest).$loaded()
+                    .then( () => {
+                        refListOfGuest.once('value', (snapshot) => {
+                            snapshot.forEach( (childSnapshot) => {
+                                const childData = childSnapshot.val();
+                                ctrl.guestData.push(childData);
+                            });
+                        });
+                        ctrl.dbLoaded = true;
+                    });
+
+                /* Komponent i18n */
+                ctrl.i18n = {
+                    overallGuestsLabel: 'Łączna liczba gości',
+                    overallChildrenAccordionHeader: 'Statystyki dot. dzieci',
+                    overallChildrenLabel: "Łączna liczba dzieci",
+                    waitingGuests: 'Goście oczekujący',
+                    confirmedGuests: 'Goście potwierdzeni',
+                    declinedGuests: 'Goście z odmową',
+                    waitingChildren: 'Dzieci ze statusem rodzica "oczekujący"',
+                    confirmedChildren: 'Dzieci ze statusem rodzica "potwierdzony"',
+                    declinedChildren: 'Dzieci ze statusem rodzica "odmowa"',
+                };
+
+            };
+        }
+    })
