b12c879b09d68d4d000ca81b55c2820e483ef764
kwielicki <krzysztofwielicki86@gmail.com>
2019-03-27 22:30:08 +0100

    Wydanie wersji 1.2.1

--- app/controllers/controller-app-version.js
+++ app/controllers/controller-app-version.js
@@ -1,5 +1,5 @@
 angular
     .module('weddingPlanner')
     .controller('controllerAppVersion', [ '$scope', function ( $scope ) {
-        $scope.appVersion = '1.2.0';
+        $scope.appVersion = '1.2.1';
     }]);
